bd50051da0fc1c6aea82eff1533c2f64712f514d
xihe <zhangxihe@bornmega.com>
2021-07-04 18:56:16 +0800

    添加页头组件

---
+++ src/components/PageHeader/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="title">{{ title }}</div>
+    <div class="content">
+      <slot name="content">{{ content }}</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageHeader",
+  props: {
+    title: {
+      type: String,
+      default: "标题",
+    },
+    content: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  margin-bottom: 20px;
+  padding: 16px 20px;
+  background: #fff;
+  border-bottom: 1px solid #e8eaec;
+
+  .title {
+    font-size: 22px;
+  }
+
+  .content {
+    color: #909399;
+    font-size: 14px;
+    margin-top: 10px;
+  }
+}
+</style>
--- src/views/icons/index.vue
+++ src/views/icons/index.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="grid">
-    <template v-for="item in svgIcons" :key="item">
-      <div class="icon-item">
-        <svg-icon :name="item" />
-        <span>{{ item }}</span>
-      </div>
-    </template>
+  <div>
+    <page-header
+      title="SVG Icon"
+      content="除了使用 element-plus 提供的图标外，你还可以使用自己的 SVG 图标"
+    />
+    <div class="grid">
+      <template v-for="item in svgIcons" :key="item">
+        <div class="icon-item">
+          <svg-icon :name="item" />
+          <span>{{ item }}</span>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 <script>
 import svgIcons from "./svg-icons";
+import PageHeader from "@/components/PageHeader";
 export default {
+  components: { PageHeader },
   setup() {
     return {
       svgIcons,
