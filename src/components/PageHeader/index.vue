21edfae037e184deb692693001c600503ec5b19e
xihe <zhangxihe@bornmega.com>
2021-07-07 13:38:37 +0800

    增加头部组件

--- src/components/PageHeader/index.vue
+++ src/components/PageHeader/index.vue
@@ -1,20 +1,11 @@
 <template>
-  <div class="header">
-    <div class="title">{{ title }}</div>
-    <div class="content">
-      <slot name="content">{{ content }}</slot>
-    </div>
-  </div>
+  <el-page-header @back="$emit('back')" :content="content" class="header" />
 </template>
 
 <script>
 export default {
   name: "PageHeader",
   props: {
-    title: {
-      type: String,
-      default: "标题",
-    },
     content: String,
   },
 };
@@ -22,19 +13,7 @@ export default {
 
 <style lang="scss" scoped>
 .header {
-  margin-bottom: 20px;
-  padding: 16px 20px;
+  padding: 26px 20px;
   background: #fff;
-  border-bottom: 1px solid #e8eaec;
-
-  .title {
-    font-size: 22px;
-  }
-
-  .content {
-    color: #909399;
-    font-size: 14px;
-    margin-top: 10px;
-  }
 }
 </style>
