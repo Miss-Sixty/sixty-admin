b08cb9cd4c5fa6ea4ddaf98f7f248d6675f35c3a
张喜贺 <zhangxihe@bornmega.com>
2021-07-31 00:01:34 +0800

    单图上传页添加提醒

--- src/views/components/imageUpload/index.vue
+++ src/views/components/imageUpload/index.vue
@@ -6,6 +6,12 @@
       title="上传"
       content="二次封装 Element Plus 的上传组件，分别封装出 单图上传、多图上传、文件上传。"
     />
+    <el-alert
+      title="上传图片时进度条没有进度的原因是因为数据请求使用mock假数据，改成正式服务器后就能看到完美的进度条了。"
+      type="warning"
+      effect="dark"
+      show-icon
+    />
     <div class="page-main">
       <tools-bar title="单图上传" :reset="false" />
       <el-row :gutter="20" justify="center">
