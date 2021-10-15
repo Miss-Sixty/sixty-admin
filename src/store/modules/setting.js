9c34caa19737e4091c465e5b94a0f055f187e02d
xihe <zhangxihe@bornmega.com>
2021-10-15 14:31:30 +0800

    修复首页部署时间显示

--- src/store/modules/setting.js
+++ src/store/modules/setting.js
@@ -1,39 +1,41 @@
-import setting from "@/setting";
-import { defineStore } from "pinia";
-import { unref } from "vue";
-import store from "@/store";
-import { useStorage } from "@vueuse/core";
-export const useSettingStore = defineStore("setting-store", {
+import setting from '@/setting'
+import { defineStore } from 'pinia'
+import { unref } from 'vue'
+import store from '@/store'
+import { useStorage } from '@vueuse/core'
+
+console.log(import.meta.env)
+export const useSettingStore = defineStore('setting-store', {
   state: () => ({
     ...setting,
-    title: import.meta.env.VITE_APP_TITLE || "", // 项目标题
-    mode: "pc",
-    collapse: useStorage("collapse", false),
-    updateTime: import.meta.env.VITE_APP_UPDATE_TIME || "未知",
+    title: import.meta.env.VITE_APP_TITLE || '', // 项目标题
+    mode: 'pc',
+    collapse: useStorage('collapse', false),
+    updateTime: __UPDATE_TIME__ || '未知',
   }),
   actions: {
     // 设置网页标题
     setTitle(state, title) {
-      state.title = title;
+      state.title = title
     },
 
     // 切换侧边栏导航展开/收起
     setCollapse() {
-      this.collapse = !this.collapse;
+      this.collapse = !this.collapse
     },
 
     // 设置访问模式，页面宽度小于 992px 时切换为移动端展示
     setMode(width) {
-      const mode = ["phone", "pad", "pc"];
-      const unWidth = unref(width);
-      this.mode = unWidth <= 768 ? mode[0] : unWidth <= 992 ? mode[1] : mode[2];
-      if (this.mode !== "pc") this.sidebarCollapse = true;
-      else this.sidebarCollapse = false;
+      const mode = ['phone', 'pad', 'pc']
+      const unWidth = unref(width)
+      this.mode = unWidth <= 768 ? mode[0] : unWidth <= 992 ? mode[1] : mode[2]
+      if (this.mode !== 'pc') this.sidebarCollapse = true
+      else this.sidebarCollapse = false
     },
   },
-});
+})
 
 // Need to be used outside the setup
 export function useSettingStoreWithOut() {
-  return useSettingStore(store);
+  return useSettingStore(store)
 }
--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,37 @@
-import { defineConfig, loadEnv } from "vite";
-import createVitePlugins from "./vite/plugins";
-import { resolve } from "path";
+import { defineConfig, loadEnv } from 'vite'
+import createVitePlugins from './vite/plugins'
+import { resolve } from 'path'
+import dayjs from 'dayjs'
 
-const fs = require("fs");
+const fs = require('fs')
 
 export default ({ mode, command }) => {
-  const env = loadEnv(mode, process.cwd());
-
+  const env = loadEnv(mode, process.cwd())
   // 全局 scss 资源
-  const scssResources = [];
-  fs.readdirSync("src/styles/resources").map((dirname) => {
+  const scssResources = []
+  fs.readdirSync('src/styles/resources').map(dirname => {
     if (fs.statSync(`src/styles/resources/${dirname}`).isFile()) {
-      scssResources.push(`@import "src/styles/resources/${dirname}";`);
+      scssResources.push(`@import "src/styles/resources/${dirname}";`)
     }
-  });
+  })
 
   return defineConfig({
-    base: "./",
-    plugins: createVitePlugins(env, command === "build"),
+    base: './',
+    plugins: createVitePlugins(env, command === 'build'),
     resolve: {
       alias: {
-        "@": resolve(__dirname, "src"),
+        '@': resolve(__dirname, 'src'),
       },
     },
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: scssResources.join(""),
+          additionalData: scssResources.join(''),
         },
       },
     },
-  });
-};
+    define: {
+      __UPDATE_TIME__: JSON.stringify(dayjs().format('YYYY-M-D HH:mm:ss')),
+    },
+  })
+}
