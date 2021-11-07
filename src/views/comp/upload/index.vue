94d5316776c5808c69ef9a0129d9f5bf97745390
xihe <im0k@outlook.com>
2021-11-07 21:21:54 +0800

    完善上传展示页

--- src/views/comp/upload/index.vue
+++ src/views/comp/upload/index.vue
@@ -1,14 +1,8 @@
 <template>
   <div>
     <page-header no-icon title="上传" content="使用 Element Plus 的上传组件，二次封装出 图片上传、文件上传组件。" />
-    <el-alert
-      title="上传图片时进度条没有进度的原因是因为数据请求使用mock假数据，改成正式服务器后就能看到完美的进度条了。"
-      type="warning"
-      effect="dark"
-      show-icon
-    />
-    <!-- <img-upload />
-    <imgs-upload /> -->
+    <img-upload />
+    <imgs-upload />
     <file-button-upload />
     <file-upload />
   </div>
