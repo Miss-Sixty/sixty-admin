757b287b96ced2b201e976a8c56cc1c7228a7576
Miss-Sixty <Miss-Sixty@users.noreply.github.com>
2021-11-07 22:07:14 +0800

    deploy: 65f72a7956af5f9c14627cb2ffcd0a698f2eacfa

--- assets/index.9d8d60cb.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index.dfa958ae.js";import{r as o,o as a,h as r,a as n}from"./vendor.283d6186.js";const s={},c={style:{padding:"30px"}};function d(l,_){const e=o("el-alert");return a(),r("div",c,[n(e,{closable:!1,title:"\u83DC\u5355 1-1",type:"warning",effect:"dark"})])}var p=t(s,[["render",d]]);export{p as default};
--- assets/index.fa7b3fbb.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./index.dfa958ae.js";import{r,o as t,h as a,a as n}from"./vendor.283d6186.js";const s={},c={style:{padding:"30px"}};function d(l,_){const e=r("el-alert");return t(),a("div",c,[n(e,{closable:!1,title:"\u83DC\u5355 1-2-2",type:"error",effect:"dark"})])}var p=o(s,[["render",d]]);export{p as default};
