ee69e7ec8bb59cd136bb7e989f24f2b4cda1651f
Miss-Sixty <Miss-Sixty@users.noreply.github.com>
2021-11-21 16:34:16 +0800

    deploy: 7d1941064986c84c6a681255c9493e2059e085c2

---
+++ assets/index.3715eda0.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.fedc5abe.js";import{r,o as t,h as a,a as n}from"./vendor.924d7bb5.js";const s={},c={style:{padding:"30px"}};function d(l,_){const e=r("el-alert");return t(),a("div",c,[n(e,{closable:!1,title:"\u83DC\u5355 1-2-1",type:"error",effect:"dark"})])}var p=o(s,[["render",d]]);export{p as default};
